6efc639491b8468dd86b29d5c7820ddc751b7c9c
tlibaiheb <tlibahouba8@gmail.com>
2024-02-15 17:01:12 +0100

    ha

---
+++ TalanPfe/TalanPfe/src/main/java/com/example/TalanPfe/TalanPfeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TalanPfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalanPfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalanPfeApplication.class, args);
+	}
+
+}
---
+++ TalanPfe/TalanPfe/src/test/java/com/example/TalanPfe/TalanPfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TalanPfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalanPfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
